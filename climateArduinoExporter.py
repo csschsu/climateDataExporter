49e57c819727b36cbf48ebd613c1b50477f2eaef
christer <christer.sundgren@systemkonstruktion.se>
2022-02-26 13:17:19 +0100

    Push feature

--- climateArduinoExporter.py
+++ climateArduinoExporter.py
@@ -11,14 +11,19 @@ from check import DataError
 from check import ds18b20_parse
 from check import dht22bmp280_parse
 
-
-registry = CollectorRegistry()
-climate = Gauge('climate_values', 'Pressure in hPa from bmp280, temp in celsius from DHT22,'
-                                  ' humidity in % from bmp280 ', ['id', 'location'], registry=registry)
-temperature = Gauge('temperature_value', 'Temperature readings from ds18b20', ['id', 'location'], registry=registry)
-
 conf = Config()
 
+# PUSH and PULL CollectorRegistry are different and can't be mixed ?!?
+# If you need both start two instances one for PULL and another for PUSH
+registry = CollectorRegistry() # Used by PUSH, PULL has :default
+if conf.CONNECTION == "PUSH":     # Setup and local instance with prometheus PUSHGATEWAY
+    climate = Gauge('climate_values', 'Pressure in hPa from bmp280, temp in celsius from DHT22,'
+                                      ' humidity in % from bmp280 ', ['id', 'location'], registry=registry)
+    temperature = Gauge('temperature_value', 'Temperature readings from ds18b20', ['id', 'location'], registry=registry)
+else :
+    climate = Gauge('climate_values', 'Pressure in hPa from bmp280, temp in celsius from DHT22,'
+                              ' humidity in % from bmp280 ', ['id', 'location'])
+    temperature = Gauge('temperature_value', 'Temperature readings from ds18b20', ['id', 'location'])
 
 def read_serial():
     ser = serial.Serial(conf.DEVICE, conf.READSPEED, timeout=conf.TIMEOUT)
@@ -35,8 +40,6 @@ def read_arduino():
 
         ds18b20_parse(buff, temperature)   # check from ds18b20 and expose as metrics
         dht22bmp280_parse(buff, climate)   # check from dht22bmp280 and expose as metrics
-
-        if conf.PUSHGATEWAY != "": push_to_gateway(conf.PUSHGATEWAY, job='batchA', registry=registry)
 
     except UnicodeError:
         print("Error reading arduino'")
@@ -56,7 +59,8 @@ if __name__ == '__main__':
     # Start up the prometheus metrics server, see
     # http://<host>:PORT to expose the metrics.
     print('Start climateArduinoExporter listening on port : ' + str(conf.PORT) + ' and arduino ' + conf.DEVICE)
-    start_http_server(conf.PORT)
+    if conf.CONNECTION != "PUSH": start_http_server(conf.PORT)  # Start a local instance queried by prometheus
     while True:
         read_arduino()
-        time.sleep(30)
+        if conf.CONNECTION == "PUSH": push_to_gateway(conf.PUSHGATEWAY, job='batchA', registry=registry)
+        time.sleep(conf.SLEEPSECONDS) # Sleep seconds ( limit: Data transfer on mobilebroadband, ie.60)
--- setup.py
+++ setup.py
@@ -8,9 +8,11 @@ class Config:
     def __init__(self):
         with open("setup.json") as json_data_file:
             data = json.load(json_data_file)
+            self.CONNECTION = data["CONNECTION"]    # One of PUSH, PULL ( registry diff )
             self.PORT = data["PORT"]                # this API endpoint
             self.DEVICE = data["DEVICE"]            # arduino device ie /dev/ttyACM0 ( uno ), /dev/ttyUSB0 (nano)
-            self.PUSHGATEWAY = data["PUSHGATEWAY"]            # http//:<host>name:9091
+            self.PUSHGATEWAY = data["PUSHGATEWAY"],  # http//:<host>name:9091
+            self.SLEEPSECONDS = data["SLEEPSECONDS"],  # time between measurements
             self.LOCATION = data["LOCATION"]        # kök
             self.BUFFERSIZE = data["BUFFERSIZE"]    # 200 bytes per read
             self.READSPEED = data["READSPEED"]      # 9600 (baud)
